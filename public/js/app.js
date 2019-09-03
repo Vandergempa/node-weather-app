c081ea227daec1348a61f50f00bf4b006355995d
Tamás Bogdán <born2shred92@gmail.com>
2019-09-03 17:33:36 +0200

    Refactoring and adding fetching data text field

--- public/js/app.js
+++ public/js/app.js
@@ -5,41 +5,45 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 const messageThree = document.querySelector('#message-3')
 
-
-weatherForm.addEventListener('submit', (e) => {
-
+const changeTextContentBefore = (e) => {
   e.preventDefault()
   messageOne.textContent = "Fetching data..."
   messageTwo.textContent = ""
   messageThree.textContent = ""
+}
+
+const changeTextContentAfter = (data) => {
+  if (data.error) {
+    messageOne.textContent = data.error
+  } else {
+    messageOne.textContent = data.latLong
+    messageTwo.textContent = data.location
+    messageThree.textContent = data.forecast
+  }
+}
+
+weatherForm.addEventListener('submit', (e) => {
+
+  changeTextContentBefore(e)
 
   fetch(`/weather?address=${input.value}`).then((response) => {
     response.json().then((data) => {
-      if (data.error) {
-        messageOne.textContent = data.error
-      } else {
-        messageOne.textContent = data.latLong
-        messageTwo.textContent = data.location
-        messageThree.textContent = data.forecast
-      }
+      changeTextContentAfter(data)
     })
   })
 })
 
-locationButton.addEventListener('click', () => {
+locationButton.addEventListener('click', (e) => {
+
+  changeTextContentBefore(e)
+
   navigator.geolocation.getCurrentPosition((position) => {
     const latitude = position.coords.latitude
     const longitude = position.coords.longitude
 
     fetch(`/weather?latitude=${latitude}&longitude=${longitude}`).then((response) => {
       response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error
-        } else {
-          messageOne.textContent = data.latLong
-          messageTwo.textContent = data.location
-          messageThree.textContent = data.forecast
-        }
+        changeTextContentAfter(data)
       })
     })
 
